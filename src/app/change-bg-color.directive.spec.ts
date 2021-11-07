bb9a149b8ff9ff4dbb4149b81f28fc85b9f3423b
Arthur Groupp <arth.groupp@gmail.com>
2021-11-07 10:20:51 +0200

    test: item component and books service (#4)

--- src/app/change-bg-color.directive.spec.ts
+++ src/app/change-bg-color.directive.spec.ts
@@ -1,8 +1,9 @@
+import { ElementRef, Renderer2 } from '@angular/core';
 import { ChangeBgColorDirective } from './change-bg-color.directive';
 
 describe('ChangeBgColorDirective', () => {
   it('should create an instance', () => {
-    const directive = new ChangeBgColorDirective();
+    const directive = new ChangeBgColorDirective({} as ElementRef, {} as Renderer2);
     expect(directive).toBeTruthy();
   });
 });
--- src/app/item/item.component.spec.ts
+++ src/app/item/item.component.spec.ts
@@ -1,6 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Item } from '../item.interface';
 
 import { ItemComponent } from './item.component';
+import { ItemModule } from './item.module';
+
+const ITEM: Item = {
+  id: 'id',
+  title: 'Dummy item',
+  imageUrl: 'URL',
+};
+
+function getButton(fixture: ComponentFixture<ItemComponent>) {
+  return fixture.debugElement.query(By.css('button'));
+}
 
 describe('ItemComponent', () => {
   let component: ItemComponent;
@@ -8,7 +21,8 @@ describe('ItemComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ItemComponent ]
+      imports: [ItemModule],
+      // declarations: [ ItemComponent ]
     })
     .compileComponents();
   });
@@ -16,10 +30,46 @@ describe('ItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ItemComponent);
     component = fixture.componentInstance;
+    fixture.componentInstance.item = ITEM;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when item is set', () => {
+    it('should show the title', () => {
+      const titleElement = fixture.debugElement.query(By.css('.title'));
+      expect(titleElement.nativeElement.textContent).toBe(ITEM.title);
+    });
+  });
+
+  describe('when item is not in the cart', () => {
+    it('should show "Add To Cart" button', () => {
+      const buttonElement = getButton(fixture);
+      expect(buttonElement.nativeElement.textContent).toBe('Add To Cart');
+    });
+  });
+
+  describe('when item is in the cart', () => {
+    beforeEach(() => {
+      fixture.componentInstance.isInCart = true;
+      fixture.detectChanges();
+    });
+
+    it('should show "Remove from Cart" button', () => {
+      const buttonElement = getButton(fixture);
+      expect(buttonElement.nativeElement.textContent).toBe('Remove from Cart');
+    });
+
+    describe('when "Remove" button clicked', () => {
+      it('should emit "removeFromCart" event with the item', () => {
+        const button = getButton(fixture);
+        spyOn(fixture.componentInstance.removeFromCart, 'emit');
+        button.nativeElement.click();
+        expect(fixture.componentInstance.removeFromCart.emit).toHaveBeenCalledWith(ITEM);
+      });
+    });
   });
 });
